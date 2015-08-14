3ffac388a8c9e56a511ba4083205047bf94d8e0d
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2015-08-14 13:56:55 -0700

    Version bump 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='taskcluster-checkout',
       package=['taskcluster-checkout'],
-      version='0.1.0',
+      version='0.1.1',
       description='Checkout cached repositories served by TaskCluster',
       author='Anthony Miyaguchi',
       author_email='amiyaguchi@mozilla.com',
